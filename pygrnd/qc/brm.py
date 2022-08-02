e938a4e46b33125000bf0c9d373fc1f5037f7400
JosQuantumThomas <thomas.decker@jos-quantum.de>
2022-08-02 15:20:36 +0200

    code improvements in file brm.py

--- pygrnd/qc/brm.py
+++ pygrnd/qc/brm.py
@@ -15,32 +15,22 @@ limitations under the License.'''
 import math
 import numpy as np
 import qiskit
-import networkx as nx
-
-## import qiskit to build circuits
 from qiskit import QuantumCircuit, QuantumRegister
 from qiskit.circuit.library.standard_gates import U3Gate
-from qiskit import execute
-from qiskit import Aer
-
-
-
 
 def brm(nodes, edges, probsNodes, probsEdges, model2gate=False):
-
-    ## input:
-    #  Risk item list e.g.  nodes = ['0','1']
-    #  Correlation risk e.g. edges=[('0','1')] # correlations
-    #  probsNodes={'0':0.1,'1':0.1} # intrinsic probs
-    #  probsEdges={('0','1'):0.2} # transition probs
-    #  output: either circuit (model2gate=False) OR gate (model2gate=True)
+    """input:
+         Risk item list e.g.  nodes = ['0','1']
+         Correlation risk e.g. edges=[('0','1')] # correlations
+         probsNodes={'0':0.1,'1':0.1} # intrinsic probs
+         probsEdges={('0','1'):0.2} # transition probs
+         output: either circuit (model2gate=False) OR gate (model2gate=True)
+    """
 
     qr=QuantumRegister(len(nodes),'q')
-    circuitname = QuantumCircuit(qr)
+    qc = QuantumCircuit(qr)
+    
     # now find RI which cannot be triggered by transitions and put uncontrolled u3 gates in for them
-    
-    
-    #import numpy as np
     mat=np.zeros((len(nodes),len(nodes)))
     for i in range(len(nodes)):
         mat[i][i]=probsNodes[nodes[i]]
@@ -48,30 +38,25 @@ def brm(nodes, edges, probsNodes, probsEdges, model2gate=False):
         for j in range(i+1,len(nodes)):
             if (nodes[i],nodes[j]) in probsEdges:
                 mat[i][j]=probsEdges[(nodes[i],nodes[j])]
-    #print(mat)
-    
     
     for x in range(len(nodes)):
         cx=0
         cv=[]
-    #       print("checking col",x)
         for y in range(len(nodes)):
             if mat[y,x] !=0 and x>y:
                 cx=1
                 cv.append(y)
         if cx==0:
-            circuitname.u(2*math.asin(math.sqrt(mat[x,x])),0,0,qr[x])
+            qc.u(2*math.asin(math.sqrt(mat[x,x])),0,0,qr[x])
         else:
             if len(cv)>1: # this RI is triggered by more than one other RI. The triggering RI are in the list "cv"
                 print("NOTE: Item",x,"is triggered by more than one other RI!")
-                #print(cv)
                 controllist=[]
                 for i in range(len(cv)):
                     controllist.append(qr[cv[i]])
                 controllist.append(qr[y])
                 for i in range(2**len(cv)):
                     cts = format(i, "0"+str(len(cv))+"b")
-                    #print(cts)
                     if i==0:
                         p = mat[y,y]
                     else:
@@ -80,28 +65,25 @@ def brm(nodes, edges, probsNodes, probsEdges, model2gate=False):
                         print("ITEM:")
                         for j in range(len(cv)):
                             if cts[j]=="1":
-                                #print("mat[y,j]",mat[j,y])
                                 p=p*(1-pbef)*mat[j,y]
                                 pbef=mat[j,y]
 
-                    circuitname.append(U3Gate(2*math.asin(math.sqrt(p)),0,0).control(num_ctrl_qubits=len(cv),ctrl_state=cts),controllist)
+                    qc.append(U3Gate(2*math.asin(math.sqrt(p)),0,0).control(num_ctrl_qubits=len(cv),ctrl_state=cts),controllist)
     # Here we can insert a loop that goes through all 2**len(cv) combinations of possibilities to control the RI and calculate the probability and put in a multiply controlled U3-gate 
             if len(cv)==0:
                 print("there's an empty risk item ...???")
             else:
-                circuitname.append(U3Gate(2*math.asin(math.sqrt(mat[x,x])),0,0).control(num_ctrl_qubits=1,ctrl_state='0'),[qr[cv[0]],qr[x]])
+                qc.append(U3Gate(2*math.asin(math.sqrt(mat[x,x])),0,0).control(num_ctrl_qubits=1,ctrl_state='0'),[qr[cv[0]],qr[x]])
                 ptrig = mat[cv[0],x] + (1-mat[cv[0],x])*mat[x,x]
-                circuitname.append(U3Gate(2*math.asin(math.sqrt(ptrig)),0,0).control(num_ctrl_qubits=1,ctrl_state='1'),[qr[cv[0]],qr[x]])
+                qc.append(U3Gate(2*math.asin(math.sqrt(ptrig)),0,0).control(num_ctrl_qubits=1,ctrl_state='1'),[qr[cv[0]],qr[x]])
     
     if model2gate==True:
-        gate=circuitname.to_gate()
+        gate=qc.to_gate()
         gate.label="BRM"
         return gate, mat
     
     if model2gate==False:
-        return circuitname, mat
-
-## classical calculation of probabilities
+        return qc, mat
 
 def findAllParents(edges, currentNode):
     res=[]
@@ -110,39 +92,37 @@ def findAllParents(edges, currentNode):
             res.append(e[0])
     return res
     
-    
-# Find all direct parents of a node that are not assigned yet.
 def findUnassignedParents(edges, currentNode, configSoFar):
+    """Find all direct parents of a node that are not assigned yet.
+    """
     res=[]
     for e in edges:
         if e[1]==currentNode and not(e[0] in configSoFar):
             res.append(e[0])
     return res
-    
+
 def findUnassignedNodes(nodes, configSoFar):
+    """Find all nodes that are not assigned yet.
+    """
     res=[]
     for n in nodes:
         if not(n in configSoFar):
             res.append(n)
     return res
     
-    
-# Find an unassigned node that has only assigned parents.
 def findCandidate(nodes, edges, configSoFar):
+    """Find an unassigned node that has only assigned parents.
+    """
     unassignedNodes=findUnassignedNodes(nodes,configSoFar)
     for n in unassignedNodes:
         if len(findUnassignedParents(edges,n,configSoFar))==0:
             return n
     return []
     
-    
-# nodes = ['0','1','2']
-# edges = [ ['0','1'],['0','2']]
-# probsNodes = {'0':0.1, '1':0.2}
-# probsEdges = {['0','1']=0.1, ['0','2']=0.2}
 def calculateProbConfiguration(nodes, edges, probsNodes, probsEdges, probSoFar, wantedConfig, configSoFar):
+    """Calculate the probability of a single configuration.
+    """
     candidate=findCandidate(nodes, edges, configSoFar)
-    #print("candidate=", candidate)
     if len(candidate)==0:
         return probSoFar
     parents=findAllParents(edges,candidate)
@@ -160,8 +140,6 @@ def calculateProbConfiguration(nodes, edges, probsNodes, probsEdges, probSoFar, 
     newDict={candidate:targetCandidate}
     return calculateProbConfiguration(nodes, edges, probsNodes, probsEdges, newProb, wantedConfig, {**configSoFar, **newDict})
     
- 
- 
 def allBinaryWords(n):
     if n==1:
         return ['0','1']
@@ -171,11 +149,47 @@ def allBinaryWords(n):
             res.append(b+'0')
             res.append(b+'1')
         return res
-        
-        
-def modelProbabilities(nodes,edges,probsNodes,probsEdges):
-    # calculateProbConfiguration(nodes, edges, probsNodes, probsEdges, probSoFar, wantedConfig, configSoFar)
 
+class NodeOfEdgeNotFoundException(Exception):
+    pass    
+
+class NodeHasNoProbability(Exception):
+    pass
+
+class EdgeHasNoProbability(Exception):
+    pass
+
+def checkGraphConsistency(nodes, edges, probsNodes, probsEdges):
+    """Make sure that all edges consist of nodes that are
+       actually in the list of nodes. This also makes
+       exception messages more readable.
+    """
+    for n in nodes:
+        if not(n in probsNodes):
+            raise NodeHasNoProbability("node "+n+" has no probability")
+    for e in edges:
+        if not ((e[0],e[1]) in probsEdges):
+            raise EdgeHasNoProbability("edge "+str(e)+" has no probability")
+        if not(e[0] in nodes):
+            raise NodeOfEdgeNotFoundException("node "+e[0]+" of edge "+e+" not in list of nodes")
+        if not(e[1] in nodes):
+            raise NodeOfEdgeNotFoundException("node "+e[1]+" of edge "+str(e)+" not in list of nodes")
+
+def modelProbabilities(nodes, edges, probsNodes, probsEdges, checkGraph=False):
+    """Compute the list of probabilities for all possible configurations of a Business Risk Model.
+       The order of the configurations is 0...0, 0...01, etc. The function also returns the sum
+       of the probabilities as a consistency check.
+
+       Example:        
+       nodes = ['0', '1', '2', '3']
+       edges = [ ('0','1'), ('1','2'), ('2','3')]
+       probsNodes = {'0':0.1, '1':0.2, '2':0.1, '3':0.0}
+       probsEdges = {('0','1'):0.1, ('1','2'):0.2, ('2','3'):0.1}
+       modelProbabilities(nodes, edges, probsNodes, probsEdges, checkGraph=True)        
+    """
+    if checkGraph==True:
+        checkGraphConsistency(nodes, edges, probsNodes, probsEdges)
+    
     words=allBinaryWords(len(nodes))
     summe=0
     states=[]
@@ -186,7 +200,6 @@ def modelProbabilities(nodes,edges,probsNodes,probsEdges):
         prob=calculateProbConfiguration(nodes, edges, probsNodes, probsEdges, 1, buffer, {})
         states.append(prob)
         summe=summe+prob
-        #print(buffer,"-> prob=",round(prob,3))
         
-    # print(summe)
     return states, summe
+
