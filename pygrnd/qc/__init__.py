bcb194ee02fd6b01a8791dcf735f3ba503d2a30a
JosQuantumThomas <thomas.decker@jos-quantum.de>
2023-03-02 13:32:56 +0100

    added modules to init file

--- pygrnd/qc/__init__.py
+++ pygrnd/qc/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['QAE','brm','brm_oracle','MaximumLikelihoodForQAEwoPE', 'optimize', 'patternGenerator', 'patternReducer', 'parallelQAE', 'helper', 'qml', 'circuitConstructor']
+__all__ = ['QAE','brm','brm_oracle','MaximumLikelihoodForQAEwoPE', 'optimize', 'patternGenerator', 'patternReducer', 'parallelQAE', 'helper', 'qml', 'circuitConstructor','lowDepthQAEgradientDescent','probabilisticNetworks']
