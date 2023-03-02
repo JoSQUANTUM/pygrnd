8b4c12e06d6a74dc792346b3c21bf8e685e12a9d
JosQuantumThomas <thomas.decker@jos-quantum.de>
2023-03-02 13:54:09 +0100

    removed files and increased version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.md') as f:
 
 setup(
     name='pygrnd',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python library for quantum algorithms and software',
     long_description_content_type="text/markdown",
     long_description=long_description,
